3d2b034fecc1f237ad4da8fbf112b50bc076db21
chandrahmuki <amouyaljerome@gmail.com>
2023-02-28 10:14:27 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,28 +1,17 @@
 !#/bin/sh
 cd ~/
-if [[ $EUID = 0 ]]; then
-    echo "Please DONT RUN AS ROOT"
-    exit
-  else (
-SUDO=''
-if [[ $EUID != 0 ]]; then
-    SUDO='sudo'
-fi
 echo "Cloning"
 git clone https://github.com/Vanilla-OS/apx.git -q
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
 echo "Setting up directories"
-$SUDO mkdir /etc/apx/
-$SUDO mkdir /usr/lib/apx/
+mkdir /etc/apx/
+mkdir /usr/lib/apx/
 echo "Compiling"
 cd ~/apx/
 go build
 echo "Moving files"
-$SUDO mv ~/apx/apx /usr/bin/
-$SUDO mv ~/apx/config/config.json /etc/apx/
-$SUDO mv ~/.local/bin/distrobox* /usr/lib/apx/
+mv ~/apx/apx /usr/bin/
+mv ~/apx/config/config.json /etc/apx/
+mv ~/.local/bin/distrobox* /usr/lib/apx/
 echo "Install Complete! :)"
-)
-fi
-
 exit
