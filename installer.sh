f9b29a6c4cf308b0f0e1825ca6671a198f17a1e3
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2023-03-05 20:53:48 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,14 +13,14 @@ git clone https://github.com/Vanilla-OS/apx.git -q
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
 echo "Setting up directories"
 $SUDO mkdir /etc/apx/
-$SUDO mkdir /usr/lib/apx/
+$SUDO mkdir /usr/local/lib/apx/
 echo "Compiling"
 cd ~/apx/
 go build
 echo "Moving files"
 $SUDO mv ~/apx/apx /usr/bin/
 $SUDO mv ~/apx/config/config.json /etc/apx/
-$SUDO mv ~/.local/bin/distrobox* /usr/lib/apx/
+$SUDO mv ~/.local/bin/distrobox* /usr/local/lib/apx/
 echo "Install Complete! :)"
 )
 fi
