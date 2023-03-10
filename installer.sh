338e5adf8c51b344f262b2e330acb64b76d18729
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2023-03-10 09:04:26 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,18 +7,12 @@ if [[ $EUID = 0 ]]; then
 
 echo "Cloning"
 git clone https://github.com/Vanilla-OS/apx.git -q
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
 echo "Setting up directories"
-sudo mkdir /etc/apx/
-sudo mkdir /usr/local/lib/apx/
 echo "Compiling"
 cd $HOME/apx/
-go build
+make build
 echo "Moving files"
-sudo mv $HOME/apx/apx $HOME/.local/bin/
-curl https://raw.githubusercontent.com/spaceguybob/apx-install/main/config.json -o $HOME/config.json
-sudo mv $HOME/config.json /etc/apx/
-sudo mv $HOME/.local/bin/distrobox* /usr/local/lib/apx/
+sudo make install PREFIX=$HOME/.local
 echo "Install Complete! :)"
 
 fi
